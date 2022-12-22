30a879b4ad1529d4cc5681514f25a2aba96c5de9
SH0420 <hantjdgh01@naver.com>
2022-12-22 18:08:33 +0900

    1

---
+++ ex00/src/main/java/com/zerock/sample/Restaurant.java
@@ -0,0 +1,21 @@
+package com.zerock.sample;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.Setter;
+
+
+@Component
+@Data
+public class Restaurant {
+			
+	//의존주입
+	@Setter(onMethod_ = @Autowired)
+	private Chef chef;
+	
+}
+	
---
+++ ex00/src/main/java/com/zerock/sample/SampleHotel.java
@@ -0,0 +1,21 @@
+package com.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Component
+@ToString
+@Getter
+@RequiredArgsConstructor
+public class SampleHotel {
+	
+	@NonNull
+	private Chef chef;
+	
+	private String name;
+	}
+
